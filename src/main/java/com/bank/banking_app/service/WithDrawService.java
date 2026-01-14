25be20c9e1f944fe5b42c10b3763f8a87a885e06
Punith <144771860+Puneeetth@users.noreply.github.com>
2026-01-14 19:54:39 +0530

    changes in withdraw

--- src/main/java/com/bank/banking_app/service/WithDrawService.java
+++ src/main/java/com/bank/banking_app/service/WithDrawService.java
@@ -16,64 +16,62 @@ import com.bank.banking_app.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import javax.naming.InsufficientResourcesException;
 import java.math.BigDecimal;
 
 @Service
 public class WithDrawService {
-    private final TransactionRepository transactionRepository;
-    private final AccountRepository accountRepository;
-    private final UserRepository userRepository;
+        private final TransactionRepository transactionRepository;
+        private final AccountRepository accountRepository;
+        private final UserRepository userRepository;
 
-    public WithDrawService(TransactionRepository transactionRepository,AccountRepository accountRepository,
-                           UserRepository userRepository){
-        this.transactionRepository = transactionRepository;
-        this.accountRepository = accountRepository;
-        this.userRepository = userRepository;
-    }
-    @Transactional
-    public void withDraw(WithDrawRequest request){
-        String email = SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getName();
-        Users user = userRepository.findByEmail(email)
-                .orElseThrow(()->new InvalidCredentialsException("User Not found"));
-
-        Account account = accountRepository.findByUser(user)
-                .orElseThrow(()->new InvalidCredentialsException("Account not found"));
-
-        if(account.getStatus() != KycStatus.AccountStatus.ACTIVE){
-            throw new AccountNotActiveException("Account is not active");
+        public WithDrawService(TransactionRepository transactionRepository, AccountRepository accountRepository,
+                        UserRepository userRepository) {
+                this.transactionRepository = transactionRepository;
+                this.accountRepository = accountRepository;
+                this.userRepository = userRepository;
         }
-        BigDecimal balanceBefore = account.getBalance();
 
-        if(balanceBefore.compareTo(request.getAmount()) < 0){
-            Transaction failedTxn = TransactionTransformer.withDrawTransaction(
-                    account,
-                    request.getAmount(),
-                    balanceBefore,
-                    balanceBefore,
-                    TransactionStatus.FAILED
+        @Transactional
+        public void withDraw(WithDrawRequest request) {
+                String email = SecurityContextHolder
+                                .getContext()
+                                .getAuthentication()
+                                .getName();
+                Users user = userRepository.findByEmail(email)
+                                .orElseThrow(() -> new InvalidCredentialsException("User Not found"));
 
-            );
-            transactionRepository.save(failedTxn);
-            throw new InsufficientBalanceException("Insufficient balance");
+                Account account = accountRepository.findByUser(user)
+                                .orElseThrow(() -> new InvalidCredentialsException("Account not found"));
+
+                if (account.getStatus() != KycStatus.AccountStatus.ACTIVE) {
+                        throw new AccountNotActiveException("Account is not active");
+                }
+                BigDecimal balanceBefore = account.getBalance();
+
+                if (balanceBefore.compareTo(request.getAmount()) < 0) {
+                        Transaction failedTxn = TransactionTransformer.withDrawTransaction(
+                                        account,
+                                        request.getAmount(),
+                                        balanceBefore,
+                                        balanceBefore,
+                                        TransactionStatus.FAILED
+
+                        );
+                        transactionRepository.save(failedTxn);
+                        throw new InsufficientBalanceException("Insufficient balance");
+                }
+                BigDecimal balanceAfter = balanceBefore.subtract(request.getAmount());
+
+                account.setBalance(balanceAfter);
+                accountRepository.save(account);
+
+                Transaction successTnx = TransactionTransformer.withDrawTransaction(
+                                account,
+                                request.getAmount(),
+                                balanceBefore,
+                                balanceAfter,
+                                TransactionStatus.SUCCESS);
+
+                transactionRepository.save(successTnx);
         }
-        BigDecimal balanceAfter = balanceBefore.subtract(request.getAmount());
-
-        account.setBalance(balanceAfter);
-        accountRepository.save(account);
-
-        Transaction successTnx = TransactionTransformer.withDrawTransaction(
-                        account,
-                request.getAmount(),
-                balanceBefore,
-                balanceAfter,
-                TransactionStatus.SUCCESS
-        );
-
-        transactionRepository.save(successTnx);
-    }
 }
